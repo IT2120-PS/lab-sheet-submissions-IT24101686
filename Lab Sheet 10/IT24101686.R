e3125d45e7c3814c35e3277812ccd5691140abb3
IT24101686 <IT24101686@my.sliit.lk>
2025-10-18 23:00:22 +0530

    Lab sheet 10

---
+++ Lab Sheet 10/IT24101686.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\powertec\\Desktop\\IT24101686")
+
+#i
+observed_counts <- c(120, 95, 85, 100)
+
+#ii
+probabilities <- c(0.25, 0.25, 0.25, 0.25)
+
+#iii
+chisq.test(x = observed_counts, p = probabilities)
